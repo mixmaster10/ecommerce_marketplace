cd8780d3a53fc6b8ec00fd00b957f31c40eaebad
mixmaster10 <smart.topdev007@gmail.com>
2021-05-12 10:32:23 +0300

    add friend page

---
+++ pages/friends/search-bar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SearchBar() {
+    return (
+        <div className="___search-bar">
+            <img src="/static/img/icons/search.png" alt="" />
+            <input type="text" placeholder="Search for a Friend" />
+        </div>
+    );
+}
+
+export default SearchBar;
